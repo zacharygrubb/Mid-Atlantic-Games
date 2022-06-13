42e3c047249a11531dea4b7ddab9cf0cfd1951a8
ZacharyGrubb <zacharygrubb@gmail.com>
2022-06-12 22:25:34 -0400

    Added Game model and admin class

--- MidAtlantic/store/admin.py
+++ MidAtlantic/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'price', 'stock')
+
+admin.site.register(Game, GameAdmin)
--- MidAtlantic/store/views.py
+++ MidAtlantic/store/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Game
 
 def index(request):
     return render(request, 'template.html')
 
 def store(request):
-    return render(request, 'store.html')
+    count = Game.objects.all().count()
+    context = {
+        'count': count,
+    }
+    return render(request, 'store.html', context)
