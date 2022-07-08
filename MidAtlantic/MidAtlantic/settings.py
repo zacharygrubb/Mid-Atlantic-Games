d46e7850a68d3b3aff93cc699f0cb329dff039d8
ZacharyGrubb <zacharygrubb@gmail.com>
2022-07-08 18:34:12 -0400

    Added static, a compressor, bootstrap, and worked on the base template

--- MidAtlantic/MidAtlantic/settings.py
+++ MidAtlantic/MidAtlantic/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'bootstrap_themes',
+    'six',
+    'compressor',
     'store',
 ]
 
@@ -122,7 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
