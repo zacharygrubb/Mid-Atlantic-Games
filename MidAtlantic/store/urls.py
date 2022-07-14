521ca1a1515b1fdbb137b263d587bc5a59d50680
ZacharyGrubb <zacharygrubb@gmail.com>
2022-07-14 19:13:16 -0400

    Added cart, revies and checkout models and the shopping cart

--- MidAtlantic/store/urls.py
+++ MidAtlantic/store/urls.py
@@ -1,6 +1,11 @@
-from django.urls import include, path
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('index', views.index, name='index'),
+    path('game/<int:game_id>', views.game_details, name='game_details'),
+    path('add/<int:game_id>', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:game_id>', views.remove_from_cart, name='remove_from_cart'),
+    path('cart', views.cart, name='cart'),
 ]
--- MidAtlantic/store/views.py
+++ MidAtlantic/store/views.py
@@ -1,12 +1,75 @@
-from django.shortcuts import render
-from .models import Game
+from django.shortcuts import render, redirect
+from .models import Game, Cart, Review, GameOrder
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def index(request):
+    return render(request, 'template.html')
+
 
 def store(request):
-    count = Game.objects.all().count()
+    games = Game.objects.all()
     context = {
-        'count': count,
+        'games': games,
     }
-    request.session['location'] = "unknown"  # Session variable
-    if request.user.is_authenticated:  # Is the user authenticated?
-        request.session['location'] = "Earth"
-    return render(request, "store.html", context)
+    return render(request, "base.html", context)
+
+
+def game_details(request, game_id):
+    context = {
+        'game': Game.objects.get(pk=game_id),
+    }
+    return render(request, 'store/detail.html', context)
+
+
+def add_to_cart(request, game_id):
+    if request.user.is_authenticated:
+        try:
+            game = Game.objects.get(pk=game_id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            try:
+                cart = Cart.objects.get(user=request.user, active=True)
+            except ObjectDoesNotExist:
+                cart = Cart.objects.create(
+                    user=request.user
+                )
+                cart.save()
+            cart.add_to_cart(game_id)
+        return redirect('cart')
+    else:
+        return redirect('index')
+
+
+def remove_from_cart(request, game_id):
+    if request.user.is_authenticated:
+        try:
+            game = Game.objects.get(pk=game_id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            cart = Cart.objects.get(user=request.user, active=True)
+            cart.remove_from_cart(game_id)
+        return redirect('cart')
+    else:
+        return redirect('index')
+
+
+def cart(request):
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user.id, active=True)[0]
+        orders = GameOrder.objects.filter(cart=cart)
+        total = 0
+        count = 0
+        for order in orders:
+            total = (order.game.price * order.quantity)
+            count = order.quantity
+        context = {
+            'cart': orders,
+            'total': total,
+            'count': count,
+        }
+        return render(request, 'store/cart.html', context)
+    else:
+        return redirect('index')
