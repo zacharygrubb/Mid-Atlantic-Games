f16d095ccbf7ed4567b24d101220ef1d5e193f54
ZacharyGrubb <zacharygrubb@gmail.com>
2022-08-10 18:49:59 -0400

    Created secure secret key and updated settings

--- MidAtlanticGames/settings.py
+++ MidAtlanticGames/settings.py
@@ -12,21 +12,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['0CZs3z2vsa577zpFQJ40k-wXGuc2kOdx6Wdd2yrFLGA']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
     'compressor',
     'store',
     'registration',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -86,7 +91,7 @@ WSGI_APPLICATION = 'MidAtlanticGames.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
