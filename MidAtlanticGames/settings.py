4b22ab264bef67587155d4e3fc353e044dc1837d
ZacharyGrubb <zacharygrubb@gmail.com>
2023-08-13 20:34:31 -0400

    moving to postgres

--- MidAtlanticGames/settings.py
+++ MidAtlanticGames/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import environ
 import django_heroku
+import dj_database_url
 
 env = environ.Env(
     SECRET_KEY=str,
@@ -100,7 +101,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
