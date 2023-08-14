a62d41cc4bc814e5f9a7276e25399dc952c99ee4
ZacharyGrubb <zacharygrubb@gmail.com>
2023-08-13 20:50:10 -0400

    settings update

--- MidAtlanticGames/settings.py
+++ MidAtlanticGames/settings.py
@@ -97,12 +97,13 @@ WSGI_APPLICATION = 'MidAtlanticGames.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
